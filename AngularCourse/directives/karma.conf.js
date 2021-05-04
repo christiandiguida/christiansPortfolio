d0f857ef4c7bb87333df3e0705a64faf5a8e97a1
Christian Di Guida <christiandiguida96@gmail.com>
2021-05-04 20:22:59 +0200

    project

--- AngularCourse/directives/karma.conf.js
+++ AngularCourse/directives/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/directives-Chris'),
+      dir: require('path').join(__dirname, './coverage/directives'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- AngularCourse/directives/src/app/app.component.spec.ts
+++ AngularCourse/directives/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'directives-Chris'`, () => {
+  it(`should have as title 'directives'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('directives-Chris');
+    expect(app.title).toEqual('directives');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('directives-Chris app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('directives app is running!');
   });
 });
